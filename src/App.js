8fd1dc5a033302c0f844d30d43b9274e15e22fe0
branherreria <branherreria@gmail.com>
2021-12-21 12:08:21 +0100

    Ya está ListaClase acabado

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Lista } from './components/Lista';
+import ListaClase from './components/ListaClase';
 
 export function App() {
   const elementos1 = [
@@ -19,7 +20,8 @@ export function App() {
       <h1>Listas de tareas</h1>
       <Lista titulo="Tareas de la casa" icono="♥" elementos={elementos1} />
       <Lista titulo="Lista de la compra" icono="✌" elementos={elementos2} />
-      <Lista titulo="ToDos del curso" icono="🙌" elementos={elementos3} />
+      <ListaClase titulo="ListaClase" icono="😭" elementos={elementos3} />
+      <ListaClase titulo="Otra lista clase" icono="😭" elementos={elementos3} />
     </div> 
   );
 }
